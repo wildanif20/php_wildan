89041c99c97ce35edd422be4c1a7de610671cb13
wildanif20 <wildanif20@gmail.com>
2022-03-31 00:01:44 +0700

    soal3

---
+++ soal3.php
@@ -0,0 +1,22 @@
+<?php
+echo "<table border='1'>";
+  echo "<tr>";
+    echo "<th>Company</th>";
+    echo "<th>Contact</th>";
+    echo "<th>Country</th>";
+  echo "</tr>";
+  echo "<tr>";
+    echo "<td>Alfreds Futterkiste</td>";
+    echo "<td>Maria Anders</td>";
+    echo "<td>Germany</td>";
+  echo "</tr>";
+echo "</table>";
+
+echo "</br>";
+echo "<form>";
+echo "Nama : <input type='text'>";
+echo "</br>";
+echo "Alamat :<input type='text'>";
+echo "</br>";
+echo "<button type='submit'> Cari";
+echo "</form>";
